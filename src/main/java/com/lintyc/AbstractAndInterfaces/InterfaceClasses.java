6f9019c8e64095bc5bd57d041c2f4ee7ec58e065
Joseph Gonzales <josephgonzales01@gmail.com>
2024-11-06 23:49:36 +1300

    more demo on interface uses

--- src/main/java/com/lintyc/AbstractAndInterfaces/InterfaceClasses.java
+++ src/main/java/com/lintyc/AbstractAndInterfaces/InterfaceClasses.java
@@ -4,7 +4,7 @@ package com.lintyc.AbstractAndInterfaces;
     1. Interface are implicitly abstract
     2. no body methods on Interface are implicitly abstract and public
     3. private methods on Interface must have a body
-    3. Interface variables are implicitly public, static, and final.
+    3. Interface variables are implicitly public, static, and final means can only declare constants.
     6. Interface may contain a default method (with body) and can be optionally overridden by implementing class
     7. Interface may contain static method, and behave just like static methods defined in classes
  */
@@ -25,41 +25,42 @@ public interface InterfaceClasses {
      */
     private void mustHaveBody() {
     }
-
-    /*
-        1. A default method may be declared only within an interface.
-        2. A default method must be marked with the default keyword and include a method body.
-        3. A default method is implicitly public.
-        4. A default method cannot be marked abstract, final, or static.
-        5. any method with a body implementation must be marked as default, unless its private
-     */
-    default double getTemperature() {
-        return 10.0;
-    }
-
-    /*
-        1. A static method must be marked with the static keyword and include a
-            method body.
-        2. A static method without an access modifier is implicitly public.
-        3. A static method cannot be marked abstract or final.
-        4. A static method is not inherited and cannot be accessed in a class implementing the
-            interface without a reference to the interface name.
-     */
-    static boolean hasTeeth() {
-        return true;
-    }
-
 }
 
 interface Herbivore {
+    /*
+       1. A default method may be declared only within an interface.
+       2. A default method must be marked with the default keyword and include a method body.
+       3. A default method is implicitly public.
+       4. A default method cannot be marked abstract, final, or static.
+       5. any method with a body implementation must be marked as default, unless its private
+    */
+    default boolean isVegan() {
+        return true;
+    }
+
     public void eatPlants();
 }
 
 interface Omnivore {
+    /*
+       1. A static method must be marked with the static keyword and include a
+           method body.
+       2. A static method without an access modifier is implicitly public.
+       3. A static method cannot be marked abstract or final.
+       4. A static method is not inherited and cannot be accessed in a class implementing the
+           interface without a reference to the interface name.
+    */
+    static boolean hasTeeth() {
+        return true;
+    }
+
     public void eatPlants();
+ 
+
 }
 
-class Bear implements Herbivore, Omnivore {
+class Bear implements Herbivore, Omnivore, InterfaceClasses {
 
     /*
         compatible to both inherited abstract methods, if one of the abstract method differ in return type this will fail
@@ -67,4 +68,36 @@ class Bear implements Herbivore, Omnivore {
     public void eatPlants() {
         System.out.println("Eating plants");
     }
+
+
+    @Override
+    public void fly(int speed) {
+        System.out.println("Im flying");
+    }
+
+    public static void main(String[] args) {
+        /*
+            It may look like it, but we are not instantiating an instance of an interface but we are instantiating
+            an anonymous class that implements Herbivore
+         */
+        Herbivore deer = new Herbivore() {
+            @Override
+            public void eatPlants() {
+                System.out.println("Only eat plants");
+            }
+        };
+        /*
+            same as above instantiation of anonymous class but this time using lambda, since we treat the Omnivore as
+            a functional interface, compiler will now look for the only abstract method to insert the println code in
+            which is in eatPlants(), if we have more than to abstract method then below lambda will fail
+         */
+        Omnivore man = () -> {
+            System.out.println("Also eat plants");
+        };
+
+        deer.isVegan();
+        deer.eatPlants();
+        man.eatPlants();
+    }
+
 }
